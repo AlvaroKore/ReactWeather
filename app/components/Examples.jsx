556f5164fb2fab86635d0bb070e4570bbc55375f
Alvaro Rene <alvaro.kom@gmail.com>
2017-09-27 17:22:40 -0500

    Add centering in Main Component

--- app/components/Examples.jsx
+++ app/components/Examples.jsx
@@ -1,4 +1,5 @@
 var React = require('react');
+var {Link} = require('react-router');
 
 // var Examples = React.createClass({
 //     render: function () {
@@ -11,8 +12,16 @@ var React = require('react');
 var Examples = (props)  => {
     return (
         <div>
-            <h3>Examples</h3>
-            <p>Welcome to examples page!</p>
+            <h1 className="text-center">Examples</h1>
+            <p>Here are a few examples locations to try out:</p>
+            <ol>
+                <li>
+                    <Link to="/?location=Ecatepec">Ecatepec</Link>
+                </li>
+                <li>
+                    <Link to="/?location=Cuernavaca">Cuernavaca</Link>
+                </li>
+            </ol>
         </div>
     );
 };
